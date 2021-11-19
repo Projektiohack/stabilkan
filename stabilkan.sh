87630c4ef34184d3cbfdbf5bdb67d0e5be39a709
Projektiohack <93693938+Projektiohack@users.noreply.github.com>
2021-11-19 18:44:43 +0700

    Add files via upload

---
+++ stabilkan.sh
@@ -0,0 +1,41 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay TIO HACK
+echo $purple "Selamat Datang"
+echo
+sleep 2
+echo $green "Pilih Tools Nya Tod"
+echo
+echo $cyan "1). Stabilkan Jaringan"
+echo
+echo $blue "0). Keluar Aja !!!"
+echo
+echo $purple
+read -p "Pilih Mana : " Tod
+
+if [ $Tod = 1 ] || [ $Tod = 1 ]
+then
+clear
+toilet -f big -F gay HAY B4B1
+echo $red "Stabilkan Jaringan Mu Tod"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $Tod = 0 ] || [ $Tod = 0 ]
+then
+clear
+echo $red
+figlet "BY TOD"
+echo $cyan "GAK USAH LEBAY"
+sleep 3
+exit
+fi
