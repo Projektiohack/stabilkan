de30ae8b44b77837dc5dc41f8a27e3388b6a8ff7
Projektiohack <93693938+Projektiohack@users.noreply.github.com>
2024-06-26 02:49:37 -0400

    ngrok
    halo cuy

---
+++ installngrok.sh
@@ -0,0 +1,106 @@
+#note script tidak boleh di ubah jika di ubah akan rusak!!
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+yellow='\033[033;1m'
+sleep 1
+echo
+toilet -f big -F gay By TIO
+echo $green  "========================================="
+echo
+echo $purple "| AUTHOR = TIO GZ NIH BOS                |"
+echo
+echo $yellow "| YT = TIO NUXZ                          |"
+echo
+echo $cyan   "| NOTE = ADA BUG LANGSUNG CHAT AUTHOR YA |"
+echo
+echo $green  "========================================="
+sleep 2
+echo $cyan"halo tante"
+echo
+echo $blue"pilih tols ya"
+echo
+echo $green"1.) menyiap kan ATACK HACK"
+echo
+echo $blue"2.) TIDAK JADI"
+echo
+echo $cyan"3.) Istall ngrok linux"
+echo
+echo $yellow"4.) Install ngrok android"
+echo
+echo $purple"5.) Lapor bug yang ada"
+echo
+echo $purple"99.) Install setup dulu bro"
+echo
+echo $yellow
+read -p"Jadi pilih mana om : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay TIO
+echo $blue"Stabilkan jaringan by tio"
+sleep 2
+ping -s1000 1.1.1.1
+fi 
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $blue
+figlet "BAY BAY"
+echo $cyan"To By Tio"
+exit
+fi 
+
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+toilet -f big -F gay PROSES
+sleep 3
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+tar -xvzf ngrok-v3-stable-linux-amd64.tgz
+clear
+toilet -f big -F gay sudah tersave ya
+$cyan "masuk kan ya token kamu"
+fi
+
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet -f big -F big gay PROSES
+sleep 4
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.tgz
+tar -xvzf ngrok-v3-stable-linux-arm64.tgz
+clear
+toilet -f big -F gay selesai ya kak
+$cyan "masukkan token kamu ya ' token ada di situs ngrok"
+fi
+
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+echo $cyan "MAKASIH BRO "
+sleep 5
+https://wa.me/6285361245464/
+fi
+
+if [ $bro = 99 ] || [ $bro = 99 ]
+then
+clear
+echo $cyan "INSTALL"
+sleep 5
+apt update
+apt upgrade
+apt install wget
+apt install toilet
+apt install bash
+apt install git
+clear
+toilet -f big -F gay selesai bro
+fi
